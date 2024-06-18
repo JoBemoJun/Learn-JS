658021e7951019538af0e208504ebf5f813cdbf3
조범준 <oreasv3@gmail.com>
2024-06-18 10:34:33 +0900

    논리 연산자 강좌

--- lectures/3_JavaScript/operator.js
+++ lectures/3_JavaScript/operator.js
@@ -58,13 +58,54 @@
  *  a /= b  => a와 b를 나눈 값을 a에 넣는다
  */
 
-let x = 10;
-let y = 10;
-x += y;
-console.log(x);
+// let x = 10;
+// let y = 10;
+// x += y;
+// console.log(x);
 
-let sum = 0;
-for (let i = 1; i <= 10; i++) {
-  sum += i;
-  console.log(`${i}번째 sum=${sum}`);
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   sum += i;
+//   console.log(`${i}번째 sum=${sum}`);
+// }
+
+// let x = true;
+// let y = true;
+// console.log(x && y); //둘다 true 일때 true
+// console.log(x || y); //둘중 하나라도 true이면 true
+// console.log(!x); //false일때 true
+
+let btn = document.querySelector('#btn');
+let emaliEl = document.querySelector('[name=email]');
+let passwordEl = document.querySelector('[name=password]');
+
+btn.addEventListener('click', function () {
+  let email = emaliEl.value;
+  let password = passwordEl.value;
+  // console.log('email: ', email);
+  // console.log('password: ', !password);
+
+  if (password && password.length === 8) {
+    console.log('성공');
+  } else {
+    console.log('실패');
+  }
+});
+//   if (email === '' || valudateEmail(email)) {
+//     console.log('이메일이 유효하지 않습니다.');
+//   } else if (!password) {
+//     console.log('비밀번호를 입력해주세요.');
+//   } else if (!(password.length >= 8 && password.length <= 16)) {
+//     console.log('비밀번호는 8~16자 이내 입니다.');
+//   } else {
+//     console.log('로그인 성공');
+//   }
+// });
+
+function valudateEmail(email) {
+  return true;
 }
+
+let number = 10;
+let message = number % 2 === 1 ? '홀수 입니다.' : '짝수 입니다.';
+console.log(message);
